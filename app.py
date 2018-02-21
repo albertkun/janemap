09664f3bc152f41e3c4aa92c823b1ae8cb4449b5
albertkun <albertk@gmx.com>
2018-02-21 04:37:17 -0800

    Revert "Revert "Merge pull request #1 from albertkun/searchbox""
    This reverts commit 12c60a9aab571b0500064e4c424965bd9e15dde3.

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from models import *
 from forms import EventHostingForm
 from geocode import geocoder
 from flask_marshmallow import Marshmallow
+from datetime import datetime
+import operator
+
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -11,12 +14,14 @@ app.config['SECRET_KEY'] = 'secret'
 ma = Marshmallow(app)
 
 
+event_types = ['Campaign Office','Official','Canvassing','Phonebanking','Voter Registration','Meeting',
+				'Fundraising','Rally','Other']
+
 class EventSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('host_name', 'email', 'event_type','event_date','lat','lon')
+        fields = ('host_name', 'email', 'event_type','event_date','url','lat','lon')
 
-user_schema = EventSchema()
 users_schema = EventSchema(many=True)
 
 
@@ -39,34 +44,18 @@ def host():
                 theField = str(field.name)
                 setattr(event,theField,field.data)
                 print getattr(event,theField)
-
-
-
-            #(event,event[theField],field.data)
-            #event.theField=field.data
-            #setattr(event[field.name],field.name,field[data])
-            # these are available to you:
-            # field.name
-            # field.description
-            # field.label.text
-            # field.data
-        # print form.event_date.data
-        # event.host_name = form.host_name.data
-        # event.event_date = form.event_date.data
-        # event.event_name = form.event_name.data
-        # event.email = form.email.data
-        # event.phone = form.phone.data
-        # person.uid = time.time()
         app.logger.info(form.data)
         event.save()
         geocoder()
         return redirect(url_for('index'))
     elif request.method =='GET':
         return render_template('host.html', form=form)
-  
+
+
+
+
 @app.route('/')
 def index():
-<<<<<<< HEAD
 	currentDay = datetime.now().date()
 	eventTypeGroups = (Event.select(Event.event_type)
 		.where(Event.event_date >= currentDay)
@@ -78,29 +67,27 @@ def index():
 		.order_by(Event.event_date.asc()))
 	db.close()
 	return render_template('index.html',events=geoevents,event_types=event_types,eventTypeGroups=eventTypeGroups)
-=======
-    geoevents = Event.select().where(Event.lat.is_null(False))
-    return render_template('index.html',events=geoevents)
->>>>>>> parent of 4258ae9... Merge pull request #1 from albertkun/searchbox
 
 @app.route('/events/<uid>')
 def uid(uid=None):
     events = Event.select().where(Event.uid == uid)
-    return render_template('event.html', events=events)  
-
-
-
+    return render_template('event.html', events=events)
 
 @app.route("/events/", methods=['GET'])
 def get_events():
     all_events = Event.select()
     result = users_schema.dump(all_events)
     return jsonify(result.data)
-
+    db.close()
 
 @app.route('/info')
 def info():
-  return render_template('info.html', events=events)  
+  return render_template('info.html', events=events)
+
+@app.teardown_request
+def _db_close(exc):
+    if not db.is_closed():
+        db.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
